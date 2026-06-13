51b8c548cd4a6a7b40d970af3685aeb6e8a13e41
Kl3XY <gaffe.martin@outlook.de>
2026-06-13 13:02:39 +0200

    feat: gets all achievements and if they are unlocked

--- CompletingWay/Plugin.cs
+++ CompletingWay/Plugin.cs
@@ -15,6 +15,7 @@ public sealed class Plugin : IDalamudPlugin
     [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
     [PluginService] internal static IClientState ClientState { get; private set; } = null!;
     [PluginService] internal static IPlayerState PlayerState { get; private set; } = null!;
+    [PluginService] internal static IUnlockState UnlockState { get; private set; } = null!;
     [PluginService] internal static IDataManager DataManager { get; private set; } = null!;
     [PluginService] internal static IPluginLog Log { get; private set; } = null!;
 
